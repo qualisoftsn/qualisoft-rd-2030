873727a6a1543554e668af1462f2f177618f052d
qualisoftsn <abdoulayethiongane@gmail.com>
2026-02-06 00:59:22 +0000

    refactor: implémentation ValidationPipe global, DTOs et sécurisation Réclamations 1.0

--- backend/src/reclamations/dto/create-reclamation.dto.ts
+++ backend/src/reclamations/dto/create-reclamation.dto.ts
@@ -3,12 +3,29 @@ import { Type } from 'class-transformer';
 import { Priority } from '@prisma/client';
 
 export class CreateReclamationDto {
-  @IsString() @IsNotEmpty() REC_Object: string;
-  @IsString() @IsNotEmpty() REC_Description: string;
-  @IsUUID() @IsNotEmpty() REC_TierId: string;
-  @IsUUID() @IsOptional() REC_ProcessusId?: string;
-  @IsEnum(Priority) @IsOptional() REC_Gravity?: Priority = Priority.MEDIUM;
-  @IsOptional() @IsString() REC_Source?: string = 'DIRECT';
+  @IsString() 
+  @IsNotEmpty() 
+  REC_Object!: string; // 👈 Le "!" dit à TS : "Cette valeur sera injectée"
+
+  @IsString() 
+  @IsNotEmpty() 
+  REC_Description!: string; // 👈 Ici aussi
+
+  @IsUUID() 
+  @IsNotEmpty() 
+  REC_TierId!: string; // 👈 Et ici
+
+  @IsUUID() 
+  @IsOptional() 
+  REC_ProcessusId?: string;
+
+  @IsEnum(Priority) 
+  @IsOptional() 
+  REC_Gravity?: Priority;
+
+  @IsOptional() 
+  @IsString() 
+  REC_Source?: string;
 
   @IsOptional()
   @Type(() => Date)
